22ac364f2ecdb7c6ff5cface44fc3859e27fe45c
it team <itsupport@telecon-systems.com>
2019-08-17 17:23:44 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from .views import *
+
+app_name = 'users'
+
+urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name="homepage.html", redirect_authenticated_user=True) \
+    	, name="homepage"),
+]
